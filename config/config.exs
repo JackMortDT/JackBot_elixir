529489a9e2d23ef2f22791cea41523ff602efe57
Luisito <le.sastre@ebc.edu.mx>
2020-09-06 22:59:34 -0500

    Configure logger info

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 use Mix.Config
 
 config :porcelain, goon_warn_if_missing: false
+config :logger, level: :info
+
+# Tokens
 config :jackbot_elixir, token: System.get_env("DISCORD_TOKEN")
-config :jackbot_elixir, cmd_prefix: "!"
+
+# Discord
+config :jackbot_elixir, discord_prefix: "!"
