53e4e3c81aaaaf1e82d179b649484d270c263b43
Luisito <le.sastre@ebc.edu.mx>
2020-09-06 20:36:51 -0500

    Changing discord command for use the bot

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :porcelain, goon_warn_if_missing: false
 config :jackbot_elixir, token: System.get_env("DISCORD_TOKEN")
-config :jackbot_elixir, cmd_prefix: "."
+config :jackbot_elixir, cmd_prefix: "!"
